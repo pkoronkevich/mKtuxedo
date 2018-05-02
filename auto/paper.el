fafd8ebfe7afa627b617e5009d291b23bcde0bff
Ben Boskin <b.boskin96@gmail.com>
2018-05-02 09:07:28 -0400

    adding RE intro

---
+++ auto/paper.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "paper"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "graphicx"
+    "enumitem"))
+ :latex)
+
